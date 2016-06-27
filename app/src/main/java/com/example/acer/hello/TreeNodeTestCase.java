18a9564e3b84ed3d8bf4c07b6b5b4ebbe59c4b8b
lenLRX <545976176@qq.com>
2016-06-27 23:05:04 +0800

    add TreeNodeTestCase

---
+++ app/src/main/java/com/example/acer/hello/TreeNodeTestCase.java
@@ -0,0 +1,16 @@
+package com.example.acer.hello;
+
+/**
+ * Created by abceq on 2016/6/27.
+ */
+public class TreeNodeTestCase {
+    public static String[] TestStrings =
+            {"/android/app/apple",
+            "/liruixin/abc/ap1",
+            "/android/app/fuck",
+            "/android/bbc/aefawf",
+            "/android/bbc/wagesgea",
+            "/android/bbc/aegaweg/sdgawge",
+            "/liruixin/abc/wagea",
+            "/liruixin/abc/fawefa/awgawe/fawfeawf/faewg"};
+}
